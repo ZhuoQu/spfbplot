00427b60d32aa8f33e804c4e6c1b3116760ee0d8
ZhuoQu <50119234+ZhuoQu@users.noreply.github.com>
2021-09-27 23:12:45 +0300

    Add files via upload

--- import_spfbplot.R
+++ import_spfbplot.R
@@ -1,3 +1,4 @@
+setwd("~/Desktop/KAUST/project/multivariate_sparse_functional_data/code")
 library(compiler)
 library(fds)
 library(dplyr)
@@ -16,5 +17,25 @@ library(doParallel)
 library(fda.usc)
 library(plotfunctions)
 library(RandomFields)
+source("calcSigma.R")
+source("facCal.R")
+source("facCal_num.R")
+source("Dirout.R")
+source("msplot_modified.R")
+
 source("sparse_fbplot_construct.R")
 source("intensity_sparse_fbplot_construct.R")
+
+fit_for_depth<-function(what,sett)
+{
+  func<-list()
+  for (i in 1:length(sett))
+  {
+    select_index<-c()
+    for (nb in 1:p)
+    {select_index<-c(select_index,i+(nb-1)*length(sett))
+    }
+    func[[i]]<-what[,select_index]
+  }
+  return (func)
+}
